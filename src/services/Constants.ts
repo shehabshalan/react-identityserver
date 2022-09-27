e889de5ce3620d0cac2ab6c77ebe2c122e5e0f0b
Shehab Shalan <shehab.shalan@hotmail.com>
2022-09-27 05:24:43 +0200

    constants

---
+++ src/services/Constants.ts
@@ -0,0 +1,9 @@
+export class Constants {
+  public static stsAuthority = process.env.stsAuthority;
+  public static clientId = process.env.clientId;
+  public static clientResponseType = process.env.clientResponseType;
+  public static clientGrantType = process.env.clientGrantType;
+  public static clientRoot = process.env.clientRoot;
+  public static clientScope = process.env.clientScope;
+  public static apiRoot = process.env.apiRoot;
+}
