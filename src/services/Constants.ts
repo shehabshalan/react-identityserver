ca5c2e196bab16b1b5250e495060a87e0a369e6f
shehabshalan <shehab.shalan@hotmail.com>
2023-02-05 14:04:59 +0100

    constants fixed

--- src/services/Constants.ts
+++ src/services/Constants.ts
@@ -1,9 +1,9 @@
 export class Constants {
-  public static stsAuthority = process.env.stsAuthority;
-  public static clientId = process.env.clientId;
-  public static clientResponseType = process.env.clientResponseType;
-  public static clientGrantType = process.env.clientGrantType;
-  public static clientRoot = process.env.clientRoot;
-  public static clientScope = process.env.clientScope;
-  public static apiRoot = process.env.apiRoot;
+  public static stsAuthority = import.meta.env.VITE_STS_AUTHORITY; // this is how vite handles env variables. docs here: https://vitejs.dev/guide/env-and-mode.html
+  public static clientId = import.meta.env.VITE_CLIENT_ID;
+  public static clientResponseType = import.meta.env.VITE_CLIENT_RESPONSE_TYPE;
+  public static clientGrantType = import.meta.env.VITE_CLIENT_GRANT_TYPE;
+  public static clientRoot = import.meta.env.VITE_CLIENT_ROOT;
+  public static clientScope = import.meta.env.VITE_CLIENT_SCOPE;
+  public static apiRoot = import.meta.env.VITE_API_ROOT;
 }
