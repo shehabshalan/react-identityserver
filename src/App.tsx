b017e3b92098a64c4481e78a88b643ec60c1c277
shehabshalan <shehab.shalan@hotmail.com>
2023-02-05 14:05:09 +0100

    clean up app

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,43 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { AuthService } from "./services/AuthService";
 import { JSONTree } from "react-json-tree";
+import type { User } from "oidc-client";
 
 function App() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState<User | null>();
 
   const authService = new AuthService();
 
   const login = async () => {
     await authService.login();
+    const user = await authService.getUser();
+    setUser(user);
   };
   const logout = async () => {
     await authService.logout();
+    setUser(null);
+  };
+
+  const handleCopy = () => {
+    if (!user) return;
+    navigator.clipboard.writeText(user.access_token);
+    alert("Copied to clipboard");
   };
 
   useEffect(() => {
-    authService.getUser().then((user) => {
-      setUser(user as Object);
-    });
+    authService
+      .getUser()
+      .then((user) => {
+        setUser(user);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
   }, []);
 
   return (
     <div className="App">
-      <h3>Hello world</h3>
+      <h3>Token Retriever</h3>
 
       <div>
         {!user && (
@@ -41,11 +56,18 @@ function App() {
       <div
         style={{ marginTop: "20px", padding: "0 5rem", borderRadius: "5px" }}
       >
-        <JSONTree
-          data={user}
-          invertTheme={true}
-          shouldExpandNode={() => true}
-        />
+        {user && (
+          <>
+            <button className="btn btn-primary" onClick={handleCopy}>
+              Copy access token
+            </button>
+            <JSONTree
+              data={user}
+              invertTheme={true}
+              shouldExpandNode={() => true}
+            />
+          </>
+        )}
       </div>
     </div>
   );
